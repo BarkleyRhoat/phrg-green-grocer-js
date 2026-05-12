22f91c6a88c27b820baafcc36006971d4794a0a4
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-05-12 09:11:06 -0400

    fist test passing

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const consolidateCart = (cart) => {
-  // code here
-}
+  return cart.reduce((counts, item) => {
+    const name = Object.keys(item)[0];
+    const { price, clearance } = item[name];
+    if (counts[name]) {
+      counts[name].count += 1;
+    } else {
+      counts[name] = { price, clearance, count: 1 };
+    }
+    return counts;
+  }, {});
+};
 
 const applyCoupons = (cart, coupons) => {
   // code here
-}
+};
 
-const applyClearance = (cart) =>{
+const applyClearance = (cart) => {
   // code here
-}
+};
 
 const checkout = (cart, coupons) => {
-  // code here
-}
+  return cart.reduce((counts, item) => {
+    const name = Object.keys(item)[0];
+    const { price, clearance } = item[name];
+    if (counts[name]) {
+      counts[name].count += 1;
+    } else {
+      counts[name] = { price, clearance, count: 1 };
+    }
+    return counts;
+  });
+};
